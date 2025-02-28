8ca91375249cc9f2c22369ff8a02c41e8c2f963e
Evalac <maslenokprograming.m@gmail.com>
2025-02-28 18:30:30 +0200

    push

--- src/pages/Popular.jsx
+++ src/pages/Popular.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 
-import { options, BASE_URL } from 'services/OptionsAPI';
+import * as API from '../services/ApiFetchServise';
 
 import Loader from 'components/Loader/Loader';
 import ResponseError from 'components/Errors/ResponseError';
 import PopularMoviesList from 'components/PopularMovies/PopularMoviesList';
+import UniversalMovieList from 'components/UniversalMoviesList/UniversalMoviesList';
+
+// movie/popular?language=en-US&page=${page}
 
 const Status = {
   IDLE: 'idle',
@@ -19,49 +22,30 @@ function Popular(params) {
   const [error, setError] = useState(null);
   const [status, setStatus] = useState(Status.IDLE);
 
-  async function fetchPopularMovies() {
-    try {
-      const responce = await fetch(
-        `${BASE_URL}/movie/popular?language=en-US&page=${page}`,
-        options
-      );
-      if (!responce.ok) {
-        throw new Error(`HTTP error! Status: ${responce.status}`);
-      }
-      const data = await responce.json();
-      return data;
-    } catch (error) {
-      throw new Error('Вибачте щось пішло не так');
-    }
-  }
-
   useEffect(() => {
-    setStatus(Status.PENDING);
-    fetchPopularMovies()
+    API.FetchMovies(`movie/popular?language=en-US`, `${page}`)
       .then(data => {
         if (page > 1) {
           setPopularMovie(prevState => [...prevState, ...data.results]);
-          setStatus(Status.RESOLVED);
           return;
         }
+
         setPopularMovie(data.results);
-        setStatus(Status.RESOLVED);
       })
-      .catch(error => {
-        setError(error);
-        setStatus(Status.REJECTED);
-      }); // eslint-disable-next-line
+      .catch(error => setError(error));
   }, [page]);
 
-  if (status === Status.PENDING) {
-    return <Loader />;
-  }
-  if (status === Status.RESOLVED) {
-    return <PopularMoviesList movieList={popularMovie} setPage={setPage} />;
-  }
-  if (status === Status.REJECTED) {
-    return <ResponseError error={error} />;
-  }
+  return <UniversalMovieList movieList={popularMovie} setPage={setPage} />;
+
+  // if (status === Status.PENDING) {
+  //   return <Loader />;
+  // }
+  // if (status === Status.RESOLVED) {
+  //   return <PopularMoviesList movieList={popularMovie} setPage={setPage} />;
+  // }
+  // if (status === Status.REJECTED) {
+  //   return <ResponseError error={error} />;
+  // }
 }
 
 export default Popular;
