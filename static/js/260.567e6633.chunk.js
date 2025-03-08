4d838340b21e6d0f7f079bb83b76d202c805bccc
Evalac <102866941+Evalac@users.noreply.github.com>
2025-03-08 18:57:51 +0000

    Deploying to gh-pages from @ Evalac/goit-react-hw-05-movies@b888eeac1fc115d3a4e0b10be1b1de51dc5e28d8 🚀

---
+++ static/js/260.567e6633.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[260],{260:function(e,t,a){a.r(t);var s=a(540),i=a(65),r=a(184);t.default=function(){var e=JSON.parse(localStorage.getItem("watchlist"));return(0,i.TH)(),(0,r.jsx)(s.Z,{movieList:e})}}}]);
+//# sourceMappingURL=260.567e6633.chunk.js.map
