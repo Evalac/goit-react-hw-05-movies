cb7d8569a894dbd8dd897c2df2f952353eb2924f
Evalac <maslenokprograming.m@gmail.com>
2025-03-08 21:07:05 +0200

    strict mode

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-05-movies">
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
