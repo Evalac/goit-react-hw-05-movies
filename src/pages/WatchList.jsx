b888eeac1fc115d3a4e0b10be1b1de51dc5e28d8
Evalac <maslenokprograming.m@gmail.com>
2025-03-08 20:57:01 +0200

    fix

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,7 +1,9 @@
 import UniversalMovieList from 'components/UniversalMoviesList/UniversalMoviesList';
+import { useLocation } from 'react-router-dom';
 
 function Watchlist() {
-  const moviesIdarr = JSON.parse(localStorage.getItem('watchlist'));
+  const moviesIdarr = JSON.parse(localStorage.getItem('watchlist')); // eslint-disable-next-line
+  const location = useLocation();
 
   return <UniversalMovieList movieList={moviesIdarr} />;
 }
