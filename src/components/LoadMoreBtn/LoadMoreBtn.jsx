4e0b6e99dde3d00d8854d220e7d66f5e7215379b
Evalac <maslenokprograming.m@gmail.com>
2025-03-21 20:21:38 +0200

    useCalback didn't work

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,16 +1,16 @@
 import css from './LoadMoreBtn.module.css';
+import { useCallback } from 'react';
 
-function LoadMoreBtn({ setPage }) {
+const LoadMoreBtn = ({ setPage }) => {
+  const handleClick = useCallback(() => {
+    setPage(prevState => prevState + 1);
+  }, [setPage]);
+
   return (
-    <button
-      className={css.load_more_btn}
-      onClick={() => {
-        setPage(prevState => prevState + 1);
-      }}
-    >
+    <button className={css.load_more_btn} onClick={handleClick}>
       Load More
     </button>
   );
-}
+};
 
 export default LoadMoreBtn;
