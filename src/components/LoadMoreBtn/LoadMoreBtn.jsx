dfbbab4a3cc923bec34dee27ebc7b234ca2bad81
Evalac <maslenokprograming.m@gmail.com>
2025-03-29 19:59:46 +0200

    scrollIntoView in button component

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,11 +1,22 @@
+import { useEffect, useRef } from 'react';
 import css from './LoadMoreBtn.module.css';
 
-function LoadMoreBtn({ setPage }) {
+function LoadMoreBtn({ setPage, page }) {
+  const buttonRef = useRef();
+
   return (
     <button
+      ref={buttonRef}
       className={css.load_more_btn}
       onClick={() => {
         setPage(prevState => prevState + 1);
+
+        setTimeout(() => {
+          buttonRef.current?.scrollIntoView({
+            block: 'start',
+            behavior: 'smooth',
+          });
+        }, 500);
       }}
     >
       Load More
